52de4a1902688fb602e11c6c04d1ca334f35044c
davevanfleet <davidvanfleet@icloud.com>
2024-05-29 12:28:12 -0400

    fix column type for alpha acid columns

---
+++ prisma/migrations/20240529162752_fix_column_type_for_alpha_acid_columns/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "hops" ALTER COLUMN "maximum_typical_alpha_acid" SET DATA TYPE DOUBLE PRECISION,
+ALTER COLUMN "minimum_typical_alpha_acid" SET DATA TYPE DOUBLE PRECISION;
