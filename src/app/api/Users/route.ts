2c9c74dddfed1988ca11dc536229c04dd5d71eed
davevanfleet <davidvanfleet@icloud.com>
2024-05-30 08:49:52 -0400

    fix: remove catch in user create

--- src/app/api/Users/route.ts
+++ src/app/api/Users/route.ts
@@ -2,16 +2,10 @@ import { prisma } from "@/utils/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const payload = await request.json()
-    console.log("payload", payload)
+  const payload = await request.json()
 
-    try {
-      const user = await prisma.user.create({data: payload});
-      const response = NextResponse.json(user);
+    const user = await prisma.user.create({data: payload});
+    const response = NextResponse.json(user);
 
-      return response
-    } catch {
-      console.error("payload from failed request", payload)
-      return NextResponse.json("invalid payload", {status: 422})
-    }
+    return response
   }
