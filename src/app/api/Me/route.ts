70deac7d0b2ec02cc4346d3f6cd9ca99d89d1f08
davevanfleet <davidvanfleet@icloud.com>
2024-06-02 07:33:49 -0400

    remove error handling in me endpoint

--- src/app/api/Me/route.ts
+++ src/app/api/Me/route.ts
@@ -5,12 +5,7 @@ import { authorize } from "../auth";
 
 export async function GET(request: NextRequest) {
     let appleId: string | undefined;
-    try {
-        appleId = authorize(request)
-    } catch (error) {
-        console.error(error)
-        return new Response(null, {status: 204})
-    }
+    appleId = authorize(request)
 
     const user = await prisma.user.findUniqueOrThrow({where: { appleId }})
     const response = NextResponse.json(user);
