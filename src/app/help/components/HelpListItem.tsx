ab646a9ee7df607a00e6a36b551b1525263b078b
davevanfleet <davidvanfleet@icloud.com>
2024-06-03 20:37:38 -0400

    feat: add boilerplate for pages

---
+++ src/app/help/components/HelpListItem.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import { Typography } from '@mui/material';
+
+type HelpListItemProps = {
+    label: string;
+    content: string
+}
+
+export const HelpListItem = ({label, content}: HelpListItemProps) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <ListItemButton onClick={handleClick}>
+        <ListItemText primary={label} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <Typography variant="caption">{content}</Typography>
+      </Collapse>
+    </>
+  );
+}
--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,9 +1,43 @@
-import { Typography } from "@mui/material";
-import { NextPage } from "next";
+'use-client'
+
+import { List, Typography } from "@mui/material";
+import { Metadata, NextPage } from "next";
+import { HelpListItem } from "./components/HelpListItem";
+
+export const metadata: Metadata = {
+  title: 'Build-a-Beer Help',
+  description: 'Support for the Build-a-Beer platform.',
+};
+
+type HelpTopic = {
+  label: string;
+  content: string;
+}
+
+const IOS_HELP_TOPICS: HelpTopic[] = [
+  {
+    label: "How do I save my recipes?",
+    content: "We currently do not support saving recipes. This feature will be added in an upcoming release. In the meantime, continue to enjoy playing around in the recipe playground!" 
+  },
+  {
+    label: "An ingredient is missing, how can I add it?",
+    content: "We have done our best to keep our app as simple as possible, and therefore we don't currently support freely adding ingredients to recipes. Future releases may include special opt-in modes for advanced brewers to create more complicated, customized recipes, but our focus will always be on keeping the app easy to use for the broader brewing community. If you feel that a commonly used ingredient is missing from our database, feel free to contact us and we will look into adding it!"
+  },
+  {
+    label: "A picture of a beer style looks wrong to me. Can you fix it?",
+    content: "Yes! please feel free to contact us and tell us what seems wrong. We will be happy to hear the feedback and update the image."
+  }
+] 
 
 const Help: NextPage = () => (
   <>
-    <Typography>Help</Typography>
+    <Typography variant="h3" gutterBottom>Help</Typography>
+    <Typography variant="h6">iOS app help</Typography>
+    <List>
+      {IOS_HELP_TOPICS.map(helpTopic => (
+        <HelpListItem key={helpTopic.label} label={helpTopic.label} content={helpTopic.content} />
+      ))}
+    </List>
   </>
 );
 
